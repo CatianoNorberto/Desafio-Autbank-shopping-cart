13d6ad3a842256b47140baa7980aba6d4adbfb3a
CatianoNorberto <catianonorberto@gmail.com>
2023-05-16 01:13:31 -0300

    novas mudanças na plataforma

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import { Rotas } from './routes'
-import { CartProvider } from './context/CartContext'
+import  { CartProvider } from './context/CartContext'
 
 function App() {
   return (
